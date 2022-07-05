de693ee262420804c73f4c46a8284c32c2a81a06
RodericoNeto <rodericocrneto@gmail.com>
2022-07-05 20:52:30 -0300

    criou a segunda forma de criar objeto

--- object.js
+++ object.js
@@ -8,3 +8,6 @@ console.log(carro)
 
 // Lista todos os nomes dos atributos dos objetos (Obs: retorna os valores dentro de um array)
 console.log(Object.keys(carro))
+
+// Lista todos os VALORES dos atributos dos objetos (Obs: retorna os valores dentro de um array)
+console.log(Object.values(carro))
---
+++ object2.js
@@ -0,0 +1,7 @@
+var carro = {};
+console.log(carro)
+console.log(typeof(carro));
+
+carro.ano = null;
+
+console.log(carro)
